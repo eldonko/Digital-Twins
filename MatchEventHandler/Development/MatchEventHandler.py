27bdb1cd19261672aac921ee861b8c7912fff555
Daniel <daniel.schatzl@stud.unileoben.ac.at>
2021-12-29 10:13:42 +0000

    removed errors

--- MatchEventHandler/Development/MatchEventHandler.py
+++ MatchEventHandler/Development/MatchEventHandler.py
@@ -1,29 +1,12 @@
 import pandas as pd
 import os
 
-
-<<<<<<< HEAD
 class MatchEventHandler:
 	def __init__(self, matches_file_path=None, match_events_file_path=None):
 		if matches_file_path is None:
-			self.matches_file_path = r"C:/Users/danie/Documents/Montanuni/2021_22WS/Digital Twins/4 Data/Matches/Matches.xls"
-=======
-class MatchEventHandler: 
-	def __init__(self, matches_file_path=None, match_events_file_path=None):
-		if matches_file_path is None:
 			self.matches_file_path = r"C:\Users\danie\Documents\Montanuni\2021_22WS\Digital Twins\4 Data\Matches\Matches.xls"
->>>>>>> 8d295a758751064cb6af81da20c50c44bf384b17
 		else:
 			self.matches_file_path = matches_file_path
-
-		if match_events_file_path is None:
-<<<<<<< HEAD
-			self.match_events_file_path = r"C:/Users/danie/Documents/Montanuni/2021_22WS/Digital Twins/4 Data/Match Events"
-=======
-			self.match_events_file_path = r"C:\Users\danie\Documents\Montanuni\2021_22WS\Digital Twins\4 Data\Match Events"
->>>>>>> 8d295a758751064cb6af81da20c50c44bf384b17
-		else:
-			self.match_events_file_path = match_events_file_path
 
 		self.matches = None # All possible matches
 		self.match = None # The match the match event data has been loaded for
